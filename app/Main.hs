7f6e6c28e67cd498779ee1ea793d9f31f10c4111
Rohith <rohithkannan007@gmail.com>
2024-02-15 21:01:45 +0530

    adding awards

--- app/Main.hs
+++ app/Main.hs
@@ -9,25 +9,36 @@
 --   print ys
 
 
-import qualified MyLib
+-- import qualified MyLib
+-- import Criterion.Main
+
+-- generateList :: Int -> [Int]
+-- generateList limit = map (\i -> replicate limit 1 `MyLib.lIndex` i) [0..limit-1]
+
+-- -- The function we're benchmarking.
+-- fib m | m < 0     = error "negative!"
+--       | otherwise = go m
+--   where
+--     go 0 = 0
+--     go 1 = 1
+--     go n = go (n-1) + go (n-2)
+
+-- -- Our benchmark harness.
+-- main = defaultMain [
+--   bgroup "Ind" [ bench "1000"  $ whnf generateList 1000
+--                , bench "10k"  $ whnf generateList 10000
+--                , bench "100k"  $ whnf generateList 100000
+--                ]
+--   ]
+
 import Criterion.Main
+import System.Environment (getArgs)
+import Awards
 
-generateList :: Int -> [Int]
-generateList limit = map (\i -> replicate limit 1 `MyLib.lIndex` i) [0..limit-1]
+benchmarkFunction :: Int -> Benchmark
+benchmarkFunction i = bench (show i) $ nf (Awards.findallawards(competitors(i `mod` 100)) i
 
--- The function we're benchmarking.
-fib m | m < 0     = error "negative!"
-      | otherwise = go m
-  where
-    go 0 = 0
-    go 1 = 1
-    go n = go (n-1) + go (n-2)
-
--- Our benchmark harness.
-main = defaultMain [
-  bgroup "Ind" [ bench "1000"  $ whnf generateList 1000
-               , bench "10k"  $ whnf generateList 10000
-               , bench "100k"  $ whnf generateList 100000
-               ]
-  ]
-
+main :: IO ()
+main = do
+    (n:_) <- getArgs
+    defaultMain [ bgroup "findallawards" [benchmarkFunction i | i <- [1..read n]] ]
